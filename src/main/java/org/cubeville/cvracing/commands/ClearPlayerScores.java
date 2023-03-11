fe27cbefd4afb522613a5130e842f2fbfda17da9
ptown16 <peytontanzillo@icloud.com>
2023-03-10 20:15:50 -0800

    undepricate get offline player

--- src/main/java/org/cubeville/cvracing/commands/ClearPlayerScores.java
+++ src/main/java/org/cubeville/cvracing/commands/ClearPlayerScores.java
@@ -12,6 +12,7 @@ import org.cubeville.cvracing.ScoreManager;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
 public class ClearPlayerScores extends BaseCommand {
 	public ClearPlayerScores() {
@@ -25,7 +26,7 @@ public class ClearPlayerScores extends BaseCommand {
 	public CommandResponse execute(CommandSender commandSender, Set<String> set, Map<String, Object> map,
 		List<Object> baseParameters) throws CommandExecutionException {
 
-		OfflinePlayer op = Bukkit.getOfflinePlayer((String) baseParameters.get(0));
+		OfflinePlayer op = Bukkit.getOfflinePlayer(UUID.fromString((String) baseParameters.get(0)));
 		if (!op.hasPlayedBefore()) {
 			throw new CommandExecutionException("Player " + baseParameters.get(0) + " does not exist.");
 		}
